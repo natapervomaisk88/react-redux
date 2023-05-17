23cf9c0d268dc4c4893edd1f9dcad8668c2e26c2
Natalya Babenko <natapervomaisk88@gmail.com>
2023-05-17 09:26:14 +0300

    added uuid

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,14 +1,24 @@
 import { useDispatch } from "react-redux";
+import { useRef } from "react";
 import { addUser } from "../redux-toolkit/slices/userSlice";
+import { v4 } from "uuid";
 
 function UserForm() {
   const dispatch = useDispatch();
-
+  const inputRef: any = useRef();
+  const handlerAddUser = () => {
+    const user = {
+      id: v4(),
+      name: inputRef.current.value,
+      isActive: true,
+    };
+    dispatch(addUser(user));
+  };
   return (
     <div>
-      UserForm
       <label>
-        Name: <input type="text" /> <button>Save</button>
+        Name: <input type="text" ref={inputRef} />{" "}
+        <button onClick={handlerAddUser}>Save</button>
       </label>
     </div>
   );
